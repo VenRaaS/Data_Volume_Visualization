2c752aa577b3f6d28c0bf787edf67704609daf78
Joshua6300018 <Joshua6300018@gmail.com>
2017-06-05 15:02:45 +0800

    Delete daliy_count.sh

--- RFM/query_script/daliy_count.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash     #宣告此script要在 bash 下執行
-
-#算每天的count
-hive -f RFM/batch/RFM_batch_script.sql -hiveconf YEAR=$(date +"%Y" --date="today") -hiveconf MONTH=$(date +"%m" --date="today") -hiveconf DATE=$(date +"%d" --date="today");
-
-
